06cfde97eb3eb17a3f78ce8377b453df513d1808
Jaesung Park <richdad6208@gmail.com>
2025-09-06 08:05:09 +0900

    ㅇ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 export default function Page() {
+  const ref = useRef<HTMLDivElement>(null);
+  const observe = new IntersectionObserver(() => console.log("hi"), {
+    root: ref.current,
+  });
+
+  useEffect(() => {
+    if (ref.current) observe.observe(ref.current);
+  }, []);
+
   return (
-    <div draggable>
+    <div className="h-[300dvh] relative">
       <img loading="lazy" src="/file.svg" alt="" className="w-10 h-10" />
       page
+      <div ref={ref} className="absolute bottom-0 w-10 h-10 bg-black"></div>
     </div>
   );
 }
